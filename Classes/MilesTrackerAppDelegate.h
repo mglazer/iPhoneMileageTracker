7fa2c95783051d48e1ee54f635dc6377aae86566
Mike Glazer <mike@imike.(none)>
2010-03-12 12:07:37 -0800

    Initial commit

---
+++ Classes/MilesTrackerAppDelegate.h
@@ -0,0 +1,29 @@
+//
+//  MilesTrackerAppDelegate.h
+//  MilesTracker
+//
+//  Created by Mike Glazer on 3/12/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+@interface MilesTrackerAppDelegate : NSObject <UIApplicationDelegate> {
+    
+    NSManagedObjectModel *managedObjectModel;
+    NSManagedObjectContext *managedObjectContext;	    
+    NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+    UIWindow *window;
+    UINavigationController *navigationController;
+}
+
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+- (NSString *)applicationDocumentsDirectory;
+
+@end
+
